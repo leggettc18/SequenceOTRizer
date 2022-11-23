e5b2196d13b80d33c2b2cb8d5a7b0bd07fa3462e
Christopher Leggett <chris@leggett.dev>
2022-11-23 15:16:09 -0500

    Adds Sequence class to ZeldaOTRizer

--- ZeldaOTRizer/Sequence.cpp
+++ ZeldaOTRizer/Sequence.cpp
@@ -1,1 +1,13 @@
 #include "Sequence.h"
+
+void ZeldaOTRizer::Sequence::WriteResourceData() {
+  writer->Write((uint32_t)size);
+  writer->Write(rawBinary.data(), size);
+  writer->Write(sequenceNum);
+  writer->Write(medium);
+  writer->Write(cachePolicy);
+  writer->Write(numFonts);
+  for (uint32_t i = 0; i < numFonts; i++) {
+    writer->Write(fontIndices[i]);
+  }
+}
