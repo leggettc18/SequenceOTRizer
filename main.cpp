0c370bcbb0eafc4fd3a417fa445124e37dcb0192
Christopher Leggett <chris@leggett.dev>
2022-11-23 16:11:12 -0500

    Stub of main function

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Sequence.h"
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        std::cout << "CLI tool for creating Custom Sequence Patch OTRs" << std::endl;
+    }
+    return 0;
+}
