336b74f57e3b68cde0f2ca64135ceac263a9f061
Christopher Leggett <chris@leggett.dev>
2022-11-23 21:45:05 -0500

    Ensures static linking and cleans up docs and clang-diag errors.

--- ZeldaOTRizer/Sequence.cpp
+++ ZeldaOTRizer/Sequence.cpp
@@ -1,8 +1,11 @@
 #include "Sequence.h"
 
+ZeldaOTRizer::Sequence::Sequence(std::shared_ptr<Ship::Archive> otrFile, const char *outPath)
+    : OTRizer::Resource(otrFile, outPath, 0x4F534551, 2, 0) {}
+
 void ZeldaOTRizer::Sequence::WriteResourceData() {
-  writer->Write((uint32_t)size);
-  writer->Write(rawBinary.data(), size);
+  writer->Write((uint32_t)this->size);
+  writer->Write(rawBinary.data(), this->size);
   writer->Write(sequenceNum);
   writer->Write(medium);
   writer->Write(cachePolicy);
